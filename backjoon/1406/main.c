438972f2fcd5cf54269d23e54a3751fa10dd102b
nini4746 <nini4746@users.noreply.github.com>
2025-12-27 20:02:45 +0900

    feat : 2문제 추가 해결

---
+++ backjoon/1406/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_node
+{
+    char c;
+    struct s_node *prev;
+    struct s_node *next;
+} t_node;
+
+t_node *new_node(char c)
+{
+    t_node *n = malloc(sizeof(t_node));
+    n->c = c;
+    n->prev = NULL;
+    n->next = NULL;
+    return n;
+}
+
+int main(void)
+{
+    char str[100001];
+    int n;
+    char cmd, ch;
+
+    /* 초기 문자열 */
+    scanf("%100000s", str);
+
+    /* 더미 헤드 */
+    t_node *head = new_node(0);
+    t_node *cursor = head;
+
+    /* 초기 문자열을 리스트로 */
+    for (int i = 0; str[i]; i++)
+    {
+        t_node *n = new_node(str[i]);
+        n->prev = cursor;
+        cursor->next = n;
+        cursor = n;
+    }
+
+    scanf("%d", &n);
+
+    while (n--)
+    {
+        scanf(" %c", &cmd);
+
+        if (cmd == 'L')
+        {
+            if (cursor != head)
+                cursor = cursor->prev;
+        }
+        else if (cmd == 'D')
+        {
+            if (cursor->next)
+                cursor = cursor->next;
+        }
+        else if (cmd == 'B')
+        {
+            if (cursor != head)
+            {
+                t_node *del = cursor;
+                cursor = cursor->prev;
+                cursor->next = del->next;
+                if (del->next)
+                    del->next->prev = cursor;
+                free(del);
+            }
+        }
+        else if (cmd == 'P')
+        {
+            scanf(" %c", &ch);
+            t_node *n = new_node(ch);
+
+            n->next = cursor->next;
+            n->prev = cursor;
+            if (cursor->next)
+                cursor->next->prev = n;
+            cursor->next = n;
+            cursor = n;
+        }
+    }
+
+    /* 출력 */
+    t_node *p = head->next;
+    while (p)
+    {
+        printf("%c", p->c);
+        t_node *tmp = p;
+        p = p->next;
+        free(tmp);
+    }
+    free(head);
+    printf("\n");
+
+    return 0;
+}
---
+++ backjoon/3273/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+int main(void)
+{
+    int n, x;
+    int cnt = 0;
+
+    scanf("%d", &n);
+
+    int *arr = malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &x);
+
+    qsort(arr, n, sizeof(int), cmp);
+
+    int l = 0;
+    int r = n - 1;
+
+    while (l < r)
+    {
+        int sum = arr[l] + arr[r];
+
+        if (sum == x)
+        {
+            cnt++;
+            l++;
+            r--;
+        }
+        else if (sum < x)
+            l++;
+        else
+            r--;
+    }
+
+    printf("%d\n", cnt);
+    free(arr);
+    return 0;
+}
