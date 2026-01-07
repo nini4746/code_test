8d732eb28190c159f7192a62d5808d06307bd688
nini4746 <nini4746@users.noreply.github.com>
2026-01-07 14:12:55 +0900

    01/07 4문제 추가 완료

---
+++ backjoon/15651/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int check(int *arr, int n)
+{
+    if (n >= 1 && arr[n] < arr[n - 1])
+            return 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[n] == arr[i])
+            return 0;
+    }
+    return (1);
+}
+
+void run(int *arr, int size, int m, int start)
+{
+    if (start == m)
+    {
+        for (int i = 0; i < m; i++)
+            printf("%d ",arr[i]);
+        printf("\n");
+        return ;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[start] = i + 1;
+        // if (!check(arr, start))
+        //     continue;
+        run (arr, size, m, start + 1);
+    }
+}
+
+int main(void)
+{
+    int n, m;
+
+    scanf("%d %d", &n, &m);
+
+    int *arr = calloc(m, sizeof(int));
+    
+    run(arr, n, m, 0);
+}
